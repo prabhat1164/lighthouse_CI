03c9d377bac91c2bda46aaedaa4280454be7da07
Prabhat Kumar <Prabhat.11911968@lpu.co.in>
2022-12-13 12:03:12 +0530

    Changes in assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,20 @@
 module.exports = {
     ci: {
+      collect: {
+        numberOfRuns: 3,
+      },
+      assert: {
+        preset: 'lighthouse:no-pwa',
+        assertions: {
+        'csp-xss': 'warn',
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.90 }],
+        },
+      },
       upload: {
-        target: 'temporary-public-storage',
+        target: "temporary-public-storage",
       },
     },
   };
