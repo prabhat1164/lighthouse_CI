a654d4df8eadc24444c1a13db6a307d3c65f0725
Prabhat Kumar <Prabhat.11911968@lpu.co.in>
2022-12-13 15:40:22 +0530

    changes in Package

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,12 +4,12 @@ module.exports = {
         target: 'temporary-public-storage',
       },
       assert: {
-        preset: 'lighthouse:no-pwa',
+        preset: 'lighthouse:recommended',
         assertions: {
           'csp-xss': 'warn',
-          'categories:performance': ['error', { minScore: 0.95 }],
-          'categories:accessibility': ['error', { minScore: 0.95 }],
-          'categories:best-practices': ['error', { minScore: 0.95 }],
+          'categories:performance': ['error', { minScore: 0.90 }],
+          'categories:accessibility': ['error', { minScore: 0.90 }],
+          'categories:best-practices': ['error', { minScore: 0.90 }],
           'categories:seo': ['error', { minScore: .90 }],
         },
       },
