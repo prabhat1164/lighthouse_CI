e7de6ab588889810dc7064a35b4344258091098d
Prabhat Kumar <Prabhat.11911968@lpu.co.in>
2022-12-14 13:06:03 +0530

    Change in about

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -7,6 +7,9 @@ export const About = () => {
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nesciunt minus perspiciatis qui fuga, nihil voluptates, reprehenderit, quisquam nulla laborum ducimus pariatur omnis ad autem illo id velit tenetur nobis sapiente eligendi dolore esse.
         </p>
+        <p>
+          Hello this is about
+        </p>
     </div>
   )
 }
